4b62f652f1f1a7b5fdf6a09c801db9f6033a8532
PasanSWijekoon <pasanwijekoon673@gmail.com>
2025-08-14 17:22:19 +0530

    Add PowerExpression class and improve formatting in CalPro.java

--- 14-quizes/2/CalPro.java
+++ 14-quizes/2/CalPro.java
@@ -5,7 +5,7 @@ interface Expression {
     int interpret(Context context);
 }
 
-class Context{
+class Context {
     private Map<String, Integer> variables = new HashMap<>();
 
     public void assign(String variable, int value) {
@@ -13,15 +13,14 @@ class Context{
     }
 
     public int lookup(String variable) {
-        if(!variables.containsKey(variable)) {
+        if (!variables.containsKey(variable)) {
             throw new IllegalArgumentException("Variable not found: " + variable);
         }
         return variables.get(variable);
     }
 }
 
-
-//terminal
+// terminal
 class NumberExpression implements Expression {
     private int number;
 
@@ -48,7 +47,7 @@ class VariableExpression implements Expression {
     }
 }
 
-//non-terminal
+// non-terminal
 class AddExpression implements Expression {
     private Expression left;
     private Expression right;
@@ -113,23 +112,40 @@ class DivisonExpression implements Expression {
     }
 }
 
+class PowerExpression implements Expression {
+    private Expression left;
+    private Expression right;
 
+    public PowerExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret(Context context) {
+        return (int) Math.pow(left.interpret(context), right.interpret(context));
+    }
+}
 
 public class CalPro {
     public static void main(String[] args) {
-       
+
         Context context = new Context();
         context.assign("x", 10);
         context.assign("y", 5);
 
-        Expression ex1 = new AddExpression(new NumberExpression(5),new NumberExpression(10));
+        Expression ex1 = new AddExpression(new NumberExpression(5), new NumberExpression(10));
         System.out.println("ADD(5, 10) = " + ex1.interpret(context));
 
-        Expression ex2 = new MultiplyExpression(new AddExpression(new NumberExpression(2), new NumberExpression(3)), new NumberExpression(4));
+        Expression ex2 = new MultiplyExpression(new AddExpression(new NumberExpression(2), new NumberExpression(3)),
+                new NumberExpression(4));
         System.out.println("MUL(ADD(2, 3), 4) = " + ex2.interpret(context));
 
-        Expression ex3 = new DivisonExpression(new SubtractExpression(new NumberExpression(20), new NumberExpression(4)), new NumberExpression(2));
+        Expression ex3 = new DivisonExpression(
+                new SubtractExpression(new NumberExpression(20), new NumberExpression(4)), new NumberExpression(2));
         System.out.println("DIV(20, 4),2)= " + ex3.interpret(context));
 
+        Expression ex4 = new PowerExpression(new NumberExpression(2), new NumberExpression(3));
+        System.out.println("POW(2, 3) = " + ex4.interpret(context)); // Output: 8
     }
 }
